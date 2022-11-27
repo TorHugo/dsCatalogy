99f3961f67b16cd7025839849d1320c20c264779
TorHugo <victor.jesus@confitec.com.br>
2022-11-27 16:48:40 -0300

    Refactor: - Adicionando seeding.

--- dscatalog/src/main/java/com/torhugo/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/torhugo/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.torhugo.dscatalog.entities.Category;
 import com.torhugo.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		
 		return repository.findAll();
