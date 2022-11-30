659150e0bf21c95743b53d49423f3d898dfae104
TorHugo <victor.jesus@confitec.com.br>
2022-11-29 22:07:58 -0300

    Feature: - Implementando method DELETE, para category; - Refatorando as exceptions personalizadas.

--- dscatalog/src/main/java/com/torhugo/dscatalog/resource/exception/ResourceExceptionHandler.java
+++ dscatalog/src/main/java/com/torhugo/dscatalog/resource/exception/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.torhugo.dscatalog.services.exceptions.DataBaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,16 +15,33 @@ import com.torhugo.dscatalog.services.exceptions.ResourceNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+	static private HttpStatus notFound = HttpStatus.NOT_FOUND;
+	static private HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException exception,
 														HttpServletRequest request){
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(notFound.value());
 		err.setError("Resource not found.");
 		err.setMessage(exception.getMessage());
 		err.setPath(request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		return ResponseEntity.status(notFound).body(err);
+	}
+
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException exception,
+														HttpServletRequest request){
+		StandardError err = new StandardError();
+
+		err.setTimestamp(Instant.now());
+		err.setStatus(badRequest.value());
+		err.setError("DataBase Exception.");
+		err.setMessage(exception.getMessage());
+		err.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(badRequest).body(err);
 	}
 }
--- dscatalog/src/main/java/com/torhugo/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/torhugo/dscatalog/services/CategoryService.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.torhugo.dscatalog.services.exceptions.DataBaseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -55,6 +58,16 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e){
 			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id not found: " + id);
+		} catch (DataIntegrityViolationException e){
+			throw new DataBaseException("Integrity violation!");
 		}
 	}
 }
