a25ac315190b50baccc6d87d67d26b83be9fa982
TorHugo <victor.jesus@confitec.com.br>
2022-11-29 21:39:51 -0300

    Feature: - Implementando method POST, para category.

--- dscatalog/src/main/java/com/torhugo/dscatalog/resource/CategoryResource.java
+++ dscatalog/src/main/java/com/torhugo/dscatalog/resource/CategoryResource.java
@@ -1,16 +1,17 @@
 package com.torhugo.dscatalog.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import com.torhugo.dscatalog.entities.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.torhugo.dscatalog.dto.CategoryDTO;
 import com.torhugo.dscatalog.services.CategoryService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -31,5 +32,16 @@ public class CategoryResource {
 	
 		CategoryDTO dto = service.findById(id);	
 		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+												.path("/{id}")
+												.buildAndExpand(
+														dto.getId()
+												).toUri();
+		return ResponseEntity.status(HttpStatus.CREATED).body(dto);
 	}
 }
